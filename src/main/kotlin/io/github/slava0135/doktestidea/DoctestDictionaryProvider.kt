779966e016ac75b480a2e2155f411017fadcfd4d
Slava0135 <super.novalskiy_0135@inbox.ru>
2023-05-09 12:21:41 +0300

    add dictionary

---
+++ src/main/kotlin/io/github/slava0135/doktestidea/DoctestDictionaryProvider.kt
@@ -0,0 +1,16 @@
+package io.github.slava0135.doktestidea
+
+import com.intellij.spellchecker.dictionary.Dictionary
+import com.intellij.spellchecker.dictionary.RuntimeDictionaryProvider
+
+class DoctestDictionaryProvider : RuntimeDictionaryProvider {
+    override fun getDictionaries() = arrayOf(DoctestDictionary())
+}
+
+class DoctestDictionary : Dictionary {
+    private val dict = DOCTEST_OPTIONS.keys + setOf("doctest")
+
+    override fun getName() = "Doktest dictionary"
+    override fun contains(word: String) = word.lowercase() in dict
+    override fun getWords() = dict.toMutableSet()
+}
