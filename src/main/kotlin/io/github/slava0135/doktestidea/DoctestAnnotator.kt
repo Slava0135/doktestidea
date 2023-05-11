1ee9fe88fc24892e59f6a6e2b0585369a215aeeb
Slava0135 <super.novalskiy_0135@inbox.ru>
2023-05-11 14:43:47 +0300

    doctest highlighter

--- src/main/kotlin/io/github/slava0135/doktestidea/DoctestAnnotator.kt
+++ src/main/kotlin/io/github/slava0135/doktestidea/DoctestAnnotator.kt
@@ -4,7 +4,10 @@ import com.intellij.lang.annotation.AnnotationHolder
 import com.intellij.lang.annotation.Annotator
 import com.intellij.lang.annotation.HighlightSeverity
 import com.intellij.openapi.editor.DefaultLanguageHighlighterColors
+import com.intellij.openapi.util.TextRange
 import com.intellij.psi.PsiElement
+import org.jetbrains.kotlin.idea.KotlinLanguage
+import org.jetbrains.kotlin.idea.highlighter.KotlinSyntaxHighlighterFactory
 
 class DoctestAnnotator : Annotator {
     override fun annotate(element: PsiElement, holder: AnnotationHolder) {
@@ -13,6 +16,23 @@ class DoctestAnnotator : Annotator {
                 .range(element.textRange)
                 .textAttributes(DefaultLanguageHighlighterColors.IDENTIFIER)
                 .create()
+            val highlighter = KotlinSyntaxHighlighterFactory.getSyntaxHighlighter(KotlinLanguage.INSTANCE, null, null)
+            val lexer = highlighter.highlightingLexer
+            lexer.start(element.text)
+            val offset = element.textRange.startOffset
+            while (lexer.tokenType != null) {
+                val range = TextRange.from(offset + lexer.tokenStart, lexer.tokenText.length)
+                val tokenHighlights = highlighter.getTokenHighlights(lexer.tokenType)
+                holder.newSilentAnnotation(HighlightSeverity.INFORMATION)
+                    .range(range)
+                    .apply {
+                        tokenHighlights.forEach {
+                            textAttributes(it)
+                        }
+                    }
+                    .create()
+                lexer.advance()
+            }
         }
     }
 }
